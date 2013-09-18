75fd0993c533f58ed45ddd30a36993c33daef339
Gino Rivera <hrivera@gi-technologies.com>
2013-09-18 14:41:54 -0400

    Copyright information

--- app/app/store/calendar/PatientSearch.js
+++ app/app/store/calendar/PatientSearch.js
@@ -1,10 +1,21 @@
 /**
- * Created with IntelliJ IDEA.
- * User: ernesto
- * Date: 5/20/13
- * Time: 11:40 AM
- * To change this template use File | Settings | File Templates.
+ * GaiaPHR (Patient Health Records)
+ * Copyright (C) 2013 Certun, inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 Ext.define('App.store.calendar.PatientSearch',{
     extend:'Ext.data.Store',
     model:'App.model.calendar.PatientSearch'
