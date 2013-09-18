1f1637946a138254f9458c9d8f80c38ff1f8d696
Ernesto J Rodriguez <vela1606@gmail.com>
2013-09-18 07:16:55 -0400

    First Commit

---
+++ portal/locales/es.js
@@ -0,0 +1,14 @@
+var locale = {
+    "reset": "Reiniciar",
+    "username": "Nombre de usuario",
+    "code": "es",
+    "please-login": "Favor de Iniciar sesión",
+    "login": "Iniciar sesión",
+    "send": "Enviar",
+    "cancel": "Cancelar",
+    "password": "Contraseña",
+
+    "site-language-error": "No puede encontrar el diccionario de su lengua, favor de verificar el parametor de lenguage",
+    "site-directory-error": "No puede encontrar el directorio de su sitio, favor de verificar el ID de su sitio",
+    "site-connection-error": "No puede conectar a su base de datos, favor de verificar el ID de su sitio"
+}
