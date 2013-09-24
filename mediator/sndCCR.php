ad03d00be76e4b575656f30560a2a607f2003b60
Gino Rivera <hrivera@gi-technologies.com>
2013-09-24 11:00:46 -0400

    Creating the mediator - code for sending CCR over the network.

--- mediator/sndCCR.php
+++ mediator/sndCCR.php
@@ -1,20 +1,85 @@
-<?
+<?php
 /**
- * GaiaPHR (Patient Health Records)
- * Copyright (C) 2013 Certun, inc.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+GaiaEHR (Electronic Health Records)
+Copyright (C) 2013 Certun, inc.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-$ServerIP = $_SERVER['REMOTE_ADDR'];
+include_once('../app/lib/Matcha/Matcha.php');
+
+class sndCCR
+{
+
+    private $hostURL;
+    private $xmlData;
+
+    /**
+     * @param $host
+     */
+    static public function setHost($host)
+    {
+        self::$hostURL = $host;
+    }
+
+    /**
+     * @param $data
+     */
+    static public function setXMLData($data)
+    {
+        self::$xmlData = $data;
+    }
+
+    /**
+     * @param $file
+     * @return bool
+     */
+    static public function loadXMLData($file)
+    {
+        try
+        {
+            self::$xmlData = file_get_contents($file);
+            return true;
+        }
+        catch(Exception $e)
+        {
+            MatchaErrorHandler::__errorProcess($e);
+            return false;
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    static public function transmitCCR()
+    {
+        try
+        {
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, self::$hostURL);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 4);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, self::$xmlData);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Connection: close'));
+            return true;
+        }
+        catch(Exception $e)
+        {
+            MatchaErrorHandler::__errorProcess($e);
+            return false;
+        }
+    }
+
+}
